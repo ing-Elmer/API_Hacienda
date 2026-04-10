f9ec4ae0aa459e2c76d113a8e40229b1f368fef8
Elmer Mejias <elmermejias47@gmail.com>
2026-04-09 22:04:45 -0600

    Update docker-entrypoint.sh

--- docker-php-apache/docker-entrypoint.sh
+++ docker-php-apache/docker-entrypoint.sh
@@ -12,7 +12,6 @@ setUpCryptoKey() {
     if [ -e "${SETTINGS_FILE_PATH}" ]; then
         echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] *** Found ${SETTINGS_FILE_PATH}, checking if cryptoKey exists ***"
 
-        # Check if cryptoKey is already set in settings.php
         if grep -q "'cryptoKey'" "${SETTINGS_FILE_PATH}"; then
             echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] CryptoKey already exists. Skipping key generation."
             return
@@ -21,7 +20,6 @@ setUpCryptoKey() {
 
     echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Generating new settings.php"
 
-    # Generate new settings.php
     cat "${SETTINGS_TEMPLATE_FILE_PATH}" \
     | sed "s/{dbName}/${CRLIBRE_API_HACIENDA_DB_NAME}/g" \
     | sed "s/{dbPss}/${CRLIBRE_API_HACIENDA_DB_PASSWORD}/g" \
@@ -43,26 +41,29 @@ setUpCryptoKey() {
     done
     echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Apache is Up"
 
-    # Generate cryptoKey if it's missing
     echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Trying to retrieve CryptoKey"
     CRLIBRE_API_HACIENDA_CRYPTO_KEY_JSON=$(curl -s "http://${LOCALHOSTNAME}:80/api.php?w=crypto&r=makeKey" -o /var/www/html/cryptoKey.json)
-    echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Retrieved JSON: ${CRLIBRE_API_HACIENDA_CRYPTO_KEY_JSON}"
     
     CRLIBRE_API_HACIENDA_CRYPTO_KEY=$(cat /var/www/html/cryptoKey.json | awk -F'"' '/"resp"/ {print $4}')
     echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] CryptoKey set to: ${CRLIBRE_API_HACIENDA_CRYPTO_KEY}"
 
-    # Replace cryptoKey in settings.php
     sed -i "s/{cryptoKey}/${CRLIBRE_API_HACIENDA_CRYPTO_KEY}/g" "${SETTINGS_FILE_PATH}"
-    
     echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Created ${SETTINGS_FILE_PATH} with CryptoKey ***"
 }
 
-# first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-        echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Starting Apache Web Server"
-        set -- apache2-foreground "$@"
+    echo "[$(date -u +%d-%m-%Y_%H-%S-%N)][${0}] Starting Apache Web Server"
+    set -- apache2-foreground "$@"
 fi
 
 setUpCryptoKey &
+
+# Fix MPM conflict
+rm -f /etc/apache2/mods-enabled/mpm_event.conf \
+      /etc/apache2/mods-enabled/mpm_event.load \
+      /etc/apache2/mods-enabled/mpm_worker.conf \
+      /etc/apache2/mods-enabled/mpm_worker.load
+ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
+ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
 
 exec "$@"
